266ca0de564a2afcd3355653ffb6004214907a07
Sean <seantierney42@gmail.com>
2018-06-19 15:24:59 -0700

    Fixing merge conflicts.

--- src/com/teamcautionrobotics/robot20XX/Robot.java
+++ src/com/teamcautionrobotics/robot20XX/Robot.java
@@ -14,84 +14,81 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the IterativeRobot documentation. If you change the name of this class
+ * or the package after creating this project, you must also update the build.properties file in the
  * project.
  */
 public class Robot extends TimedRobot {
-	private static final String kDefaultAuto = "Default";
-	private static final String kCustomAuto = "My Auto";
-	private String m_autoSelected;
-	private SendableChooser<String> m_chooser = new SendableChooser<>();
-	Joystick leftJoystick;
-	Joystick rightJoystick;
-	DriveBase robotDrive;
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private SendableChooser<String> m_chooser = new SendableChooser<>();
+    Joystick leftJoystick;
+    Joystick rightJoystick;
+    DriveBase robotDrive;
 
-	/**
-	 * This function is run when the robot is first started up and should be used
-	 * for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		m_chooser.addDefault("Default Auto", kDefaultAuto);
-		m_chooser.addObject("My Auto", kCustomAuto);
-		SmartDashboard.putData("Auto choices", m_chooser);
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.addDefault("Default Auto", kDefaultAuto);
+        m_chooser.addObject("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
 
-		robotDrive = new DriveBase(0, 1);
-		leftJoystick = new Joystick(0);
-		rightJoystick = new Joystick(1);
-	}
+        robotDrive = new DriveBase(0, 1);
+        leftJoystick = new Joystick(0);
+        rightJoystick = new Joystick(1);
+    }
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable chooser
-	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-	 * remove all of the chooser code and uncomment the getString line to get the
-	 * auto name from the text box below the Gyro
-	 *
-	 * <p>
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the SendableChooser
-	 * make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		m_autoSelected = m_chooser.getSelected();
-		// m_autoSelected = SmartDashboard.getString("Auto Selector",
-		// kDefaultAuto);
-		System.out.println("Auto selected: " + m_autoSelected);
-	}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select between different
+     * autonomous modes using the dashboard. The sendable chooser code works with the Java
+     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+     * uncomment the getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the switch structure
+     * below with additional strings. If using the SendableChooser make sure to add them to the
+     * chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // m_autoSelected = SmartDashboard.getString("Auto Selector",
+        // kDefaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-		case kCustomAuto:
-			// Put custom auto code here
-			break;
-		case kDefaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
-	}
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
 
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	@Override
-	public void teleopPeriodic() {
-		robotDrive.drive(leftJoystick.getY(), rightJoystick.getY());
-	}
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        robotDrive.drive(leftJoystick.getY(), rightJoystick.getY());
+    }
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {}
 }
